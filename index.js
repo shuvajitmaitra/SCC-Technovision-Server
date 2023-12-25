821923b718712c7a2072ef5953acb599514181df
Shuvajit Maitra <shuvajitmaitra@gmail.com>
2023-12-25 21:04:39 +0600

    post api create

--- index.js
+++ index.js
@@ -23,8 +23,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
+
 
 
     const todoCollection = client.db("SCCTechnovisionDB").collection("todo");
